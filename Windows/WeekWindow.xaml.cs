d4f3b384d25b4a9f9c28b47a5ba88476c735d120
Guillian VIBERT-VALLET <guillian.vibert-vallet@boondooa.com>
2023-03-29 17:01:07 +0200

    feat: Add new window with week choosing

---
+++ Windows/WeekWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailCreator.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour WeekWindow.xaml
+    /// </summary>
+    public partial class WeekWindow : UserControl
+    {
+        public WeekWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
