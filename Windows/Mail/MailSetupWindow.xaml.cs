8b2f4efe180ba297edbb1c56cb90df1ae6811cd9
Guillian VIBERT-VALLET <guillian.vibert-vallet@boondooa.com>
2023-04-13 17:08:30 +0200

    feat: add text color changing on string match

--- Windows/Mail/MailSetupWindow.xaml.cs
+++ Windows/Mail/MailSetupWindow.xaml.cs
@@ -31,5 +31,104 @@ namespace MailCreator.Windows.Mail
             this.Content = homeWindow;
         }
 
+        private void ChangeTextColor(string[] textsToFind, SolidColorBrush color)
+        {
+            //if (fdMailBody == null || pgMailBody == null) return;
+
+            FlowDocument document = rtbMailBody.Document;
+
+            // Vérifier si le FlowDocument est nul
+            if (document == null) return;
+
+            // Récupérer la liste des paragraphes
+            List<Paragraph> paragraphs = new List<Paragraph>();
+            document.Blocks.ToList().ForEach(b =>
+            {
+                if (b is Paragraph)
+                    paragraphs.Add((Paragraph)b);
+            });
+
+            foreach (Paragraph paragraph in paragraphs)
+            {
+                paragraph.LineHeight = 7;
+                paragraph.Inlines.ToList().ForEach(inline =>
+                {
+                    if (inline.GetType() == typeof(Run))
+                    {
+
+
+
+                        Run run = (Run)inline;
+                     
+                        foreach (string textToFind in textsToFind)
+                        {
+
+
+
+                            if (run.Text.Contains(textToFind))
+                            {
+
+                                if (run.Text == textToFind)
+                                {
+                                    run.Foreground = color;
+                                    return;
+                                }
+
+                                int initialRunId = paragraph.Inlines.ToList().IndexOf(inline);
+
+                                List<Run> runList = new List<Run>();
+
+                                string[] textSplited = run.Text.Split(new string[] { textToFind }, StringSplitOptions.RemoveEmptyEntries);
+
+                                if (textSplited.Length > 0)
+                                {
+
+                                    for (int i = 0; i < textSplited.Length; i++)
+                                    {
+                                        runList.Add(new Run(textSplited[i]));
+                                        if (i != textSplited.Length - 1 || textSplited.Length == 1)
+                                        {
+                                            Run runTextFind = new Run(textToFind);
+                                            runTextFind.Foreground = color;
+                                            runList.Add(runTextFind);
+                                        }
+                                    }
+                                    TextPointer pointer = rtbMailBody.Document.ContentEnd;
+                                    runList.ForEach(Localrun =>
+                                    {
+                                        paragraph.Inlines.InsertBefore(inline, Localrun);
+                                        pointer = Localrun.ContentEnd;
+                                    });
+                                    paragraph.Inlines.Remove(inline);
+
+                                    pointer = rtbMailBody.Document.ContentEnd;
+                                }
+
+
+
+                            }
+                            else
+                                run.Foreground = new SolidColorBrush(Colors.Black);
+                        }
+                    }
+                });
+
+            }
+
+
+
+
+
+        }
+
+        private void RichTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            ChangeTextColor(new string[]
+            {
+                "$nomMatiere$",
+                "$nomEnseignant$",
+            }, new SolidColorBrush(Colors.ForestGreen));
+
+        }
     }
 }
